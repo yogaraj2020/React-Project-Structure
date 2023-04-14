a2cecd7d161bdf5c615f023c4107a3693a76f3cd
yogaraj2020 <60388849+yogaraj2020@users.noreply.github.com>
2023-04-14 13:22:15 -0400

    Auth related
    Auth related

--- auth.js
+++ auth.js
@@ -1,24 +1,77 @@
-import { PublicClientApplication } from '@azure/msal-browser';
+import * as msal from 'msal';
 
-export const authConfig = {
-  clientId: '<your_client_id>',
-  redirectUri: '<your_redirect_uri>',
-  authority: 'https://login.microsoftonline.com/<your_tenant_id>',
-  scopes: [
-    '<your_scope_1>',
-    '<your_scope_2>',
-    // ...
-  ],
-};
-
-export const msalInstance = new PublicClientApplication({
+// MSAL Configuration
+const msalConfig = {
   auth: {
-    clientId: authConfig.clientId,
-    redirectUri: authConfig.redirectUri,
-    authority: authConfig.authority,
+    clientId: 'your-client-id',
+    authority: 'https://login.microsoftonline.com/your-tenant-id',
+    redirectUri: window.location.origin,
   },
   cache: {
     cacheLocation: 'localStorage',
-    storeAuthStateInCookie: true,
+    storeAuthStateInCookie: false,
   },
-});
+};
+
+const loginRequest = {
+  scopes: ['openid', 'profile', 'user.read'],
+};
+
+// Create MSAL instance
+const msalInstance = new msal.PublicClientApplication(msalConfig);
+
+// Function to handle MSAL redirect callback
+export const handleRedirectCallback = () => {
+  return msalInstance.handleRedirectPromise();
+};
+
+// Function to start MSAL login flow
+export const login = async () => {
+  try {
+    await msalInstance.loginRedirect(loginRequest);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Function to get the user's access token
+export const getAccessToken = async () => {
+  try {
+    const accounts = await msalInstance.getAllAccounts();
+    if (accounts.length === 0) {
+      throw new Error('No accounts found');
+    }
+    const silentRequest = {
+      scopes: ['openid', 'profile', 'user.read'],
+      account: accounts[0],
+    };
+    const authResult = await msalInstance.acquireTokenSilent(silentRequest);
+    return authResult.accessToken;
+  } catch (error) {
+    if (error instanceof msal.InteractionRequiredAuthError) {
+      const interactiveRequest = {
+        scopes: ['openid', 'profile', 'user.read'],
+      };
+      const authResult = await msalInstance.acquireTokenRedirect(interactiveRequest);
+      return authResult.accessToken;
+    } else {
+      console.error(error);
+    }
+  }
+};
+
+// Function to check if user is authenticated
+export const isAuthenticated = () => {
+  const accounts = msalInstance.getAllAccounts();
+  return accounts.length > 0;
+};
+
+// Function to check if user is authorized based on roles
+export const isAuthorized = (roles) => {
+  const account = msalInstance.getAccount();
+  if (!account) {
+    return false;
+  }
+  const accountRoles = account.idTokenClaims.roles || [];
+  return roles.some((role) => accountRoles.includes(role));
+};
---
+++ routeConfig.js
@@ -0,0 +1,23 @@
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+import AdminPage from './pages/AdminPage';
+import { isAuthenticated, isAuthorized } from './auth';
+
+const routes = [
+  {
+    path: '/login',
+    component: LoginPage,
+  },
+  {
+    path: '/',
+    component: HomePage,
+    canActivate: [isAuthenticated],
+  },
+  {
+    path: '/admin',
+    component: AdminPage,
+    canActivate: [isAuthenticated, isAuthorized(['admin'])],
+  },
+];
+
+export default routes;
